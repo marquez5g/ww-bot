670f440bcdd90a9063f5baa763f51dbc6cc1690b
Gabriel Marquez <marquez122@outlook.com>
2021-07-10 18:35:05 -0500

    adds nodemon to reload server on file changes, session handling by saving tokens to session.json to remeber the session on server start, !pong and !hour commands

--- app.js
+++ app.js
@@ -7,9 +7,33 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const fs = require('fs');
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
+
+// Path where the session data will be stored
+const SESSION_FILE_PATH = './session.json';
+
+// Load the session data if it has been previously saved
+let sessionData;
+if(fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
+
+// Use the saved values
+const client = new Client({
+  session: sessionData
+});
+
+// Save session values to the file upon successful auth
+client.on('authenticated', (session) => {
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+      if (err) {
+          console.error(err);
+      }
+  });
+});
 
 client.on('qr', qr => {
   qrcode.generate(qr, {small: true});
@@ -17,6 +41,19 @@ client.on('qr', qr => {
 
 client.on('ready', () => {
   console.log('Client is ready!');
+});
+
+client.on('message', message => {
+  console.log(message.body);
+	if(message.body === '!ping') {
+		message.reply('pong');
+	} else if (message.body === '!hora') {
+    let today = new Date();
+    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let dateTime = date+' '+time;
+    message.reply(dateTime);
+  }
 });
 
 client.initialize();
