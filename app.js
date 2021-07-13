3ce0f5a6fec98e730b5e02bd083488e96faa8a77
David256 <wasterdavid@gmail.com>
2021-07-12 22:36:41 -0500

    Prune unnecessary code

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 const fs = require('fs');
 const qrcode = require('qrcode-terminal');
+const debug = require('debug')('ww-bot');
 const { Client } = require('whatsapp-web.js');
 
 // Path where the session data will be stored
@@ -16,77 +8,46 @@ const SESSION_FILE_PATH = './session.json';
 
 // Load the session data if it has been previously saved
 let sessionData;
-if(fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  // eslint-disable-next-line global-require
+  sessionData = require(SESSION_FILE_PATH);
 }
 
 // Use the saved values
 const client = new Client({
-  session: sessionData
+  session: sessionData,
 });
 
 // Save session values to the file upon successful auth
 client.on('authenticated', (session) => {
   sessionData = session;
-  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-      if (err) {
-          console.error(err);
-      }
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
   });
 });
 
-client.on('qr', qr => {
-  qrcode.generate(qr, {small: true});
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-  console.log('Client is ready!');
+  debug('Client is ready!');
 });
 
-client.on('message', message => {
-  console.log(message.body);
-	if(message.body === '!ping') {
-		message.reply('pong');
-	} else if (message.body === '!hora') {
-    let today = new Date();
-    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    let dateTime = date+' '+time;
+client.on('message', (message) => {
+  debug(message.body);
+  if (message.body === '!ping') {
+    message.reply('pong');
+  } else if (message.body === '!hora') {
+    const today = new Date();
+    const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+    const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+    const dateTime = `${date} ${time}`;
     message.reply(dateTime);
   }
 });
 
 client.initialize();
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
